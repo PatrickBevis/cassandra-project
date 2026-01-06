341cb76e150c7faf0a7f5e48cb538e38b5070a6a
Patrick <bevis.patrick@gmail.com>
2026-01-06 15:46:04 +0100

    regex

--- src/Form/CustomerType.php
+++ src/Form/CustomerType.php
@@ -6,14 +6,31 @@ use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class CustomerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
+            // ->add('name')
+            // ->add('email')
+            
+              ->add('name', TextType::class, [
+        'label' => 'Nom',
+        'attr' => [
+            'maxlength' => 100,
+            'placeholder' => 'Ex: Marie Dupont',
+        ],
+    ])
+    ->add('email', EmailType::class, [
+        'label' => 'Email',
+        'attr' => [
+            'maxlength' => 100,
+            'placeholder' => 'ex: marie@domaine.fr',
+        ],
+    ]);
         ;
     }
 
