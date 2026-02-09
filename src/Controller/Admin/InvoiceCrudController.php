d799dbfdffbed04b2f1be4c72786d87e7068a819
Patrick <bevis.patrick@gmail.com>
2026-02-09 09:53:18 +0100

    invoice update

--- src/Controller/Admin/InvoiceCrudController.php
+++ src/Controller/Admin/InvoiceCrudController.php
@@ -4,17 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Invoice;
 use App\Enum\InvoiceStatus;
-use Doctrine\DBAL\Types\DecimalType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
 
 
 class InvoiceCrudController extends AbstractCrudController
@@ -43,7 +39,6 @@ class InvoiceCrudController extends AbstractCrudController
                 $invoice->getTaxes()->value
             ),
             NumberField::new('priceWithTax')->setDecimalSeparator(','),
-            NumberField::new('total'),
             
         ];
     }
--- src/Validator/Regex.php
+++ src/Validator/Regex.php
@@ -8,4 +8,6 @@ final class Regex
     public const EMAIL = '/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i';
     public const SIRET = '/\d{14}/';
     public const PHONE = '/(?:([+]\d{1,4})[-.\s]?)?(?:[(](\d{1,3})[)][-.\s]?)?(\d{1,4})[-.\s]?(\d{1,4})[-.\s]?(\d{1,9})/';
+
+
 }
