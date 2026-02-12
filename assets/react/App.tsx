c1c75dbb90f185001edcc79f21b5dae6a4cc3645
Patrick <bevis.patrick@gmail.com>
2026-02-12 19:33:53 +0100

    react2

---
+++ assets/react/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App(){
+     return (
+    <div>
+      <h1>React in Symfony</h1>
+      <p>Symfony rend le Twig, React rend l'UI ici.</p>
+    </div>
+  );
+}
