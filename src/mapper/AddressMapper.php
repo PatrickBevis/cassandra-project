5414443c8e92179309ad72eb4926b7ef1c75bbf5
Patrick <bevis.patrick@gmail.com>
2026-03-18 15:30:55 +0100

    feat: mapper Address

---
+++ src/mapper/AddressMapper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Mapper;
+
+use App\DTO\AddressDTO;
+
+use App\Entity\Address;
+
+class ProduitMapper
+{
+    public function __construct(
+ 
+    ) {}
+    public function toDto(Address $address): AddressDTO
+    {
+        
+        return new AddressDTO(
+            id: $address->getId(),
+            street_number: $address->getStreetNumber(),
+            street_way: $address->getStreetWay(),
+            street_name: $address->getStreetName(),
+            street_complementary: $address->getStreetComplementary(),
+            zip: $address->getZip(),
+            city: $address->getCity(),
+            country: $address->getCountry(),
+            is_EU: $address->isEU(),
+        );
+    }
+}
